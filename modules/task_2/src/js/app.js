e68781855badb0df3fb99c7fc39a3e35cd015cfc
Roman Prilepskiy <r616on@gmail.com>
2021-04-16 22:30:10 +0300

    init

--- modules/task_2/src/js/app.js
+++ modules/task_2/src/js/app.js
@@ -1,6 +1,2 @@
-// TODO: write your code here
-import sum from './basic';
-
-console.log('worked');
-
-console.log(sum([1, 2]));
+const game = new Game();
+game.start();
---
+++ modules/task_2/src/js/game.js
@@ -0,0 +1,18 @@
+import Character from './domain.js';
+
+export { GameSavingData, readGameSaving, writeGameSaving };
+
+export default class Game {
+   start() {
+      console.log('game started');
+   }
+}
+
+class GameSavingData {
+}
+
+function readGameSaving() {
+}
+
+function writeGameSaving() {
+}
